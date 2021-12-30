ed4989d105663daab4a274f81f029fc06dedc4f8
Michael Stetner <michael.stetner@gmail.com>
2021-12-29 19:32:27 -0700

    day 01 part 2

--- day/01/day01_part1.py
+++ day/01/day01_part1.py
@@ -3,27 +3,52 @@ import sys
 import pyspark
 import pyspark.sql.functions as f
 
-# Usual setup
-input_filename = '/Users/stetner/adventofcode/2021/day/01/input.txt' #sys.argv[1]
-spark = pyspark.sql.SparkSession.builder.getOrCreate()
+def load_data(spark, filename):
+    # Read data, which is one number per line representing the depth of the ocean
+    input_data = spark.read.csv(input_filename, schema='depth INT')
 
-# Read data, which is one number per line representing the depth of the ocean
-input_data = spark.read.csv(input_filename, schema='depth INT')
+    # I want to keep the data in the original order, so I need to add a column with
+    # the original sequence
+    return input_data.withColumn('seq', f.monotonically_increasing_id())
 
-# My strategy is to enrich each depth with the "previous depth".
-# Then, I'll count the rows where depth is greater than previous depth
+def count_increases(data, col='depth', order_by='seq'):
+    # My strategy is to enrich each depth with the "previous depth".
+    # Then, I'll count the rows where depth is greater than previous depth
+    # Doing the "previous depth" requires some setup. I need to use the lag()
+    # function, but that requires a window with orderBy().
+    w = pyspark.sql.Window.orderBy(order_by)
+    prev_col = 'prev_' + col
+    data = data.withColumn(prev_col, f.lag(data[col]).over(w))
+    return data.filter(data[col] > data[prev_col]).count()
 
-# Doing the "previous depth" requires some setup. I need to use the lag()
-# function, but that requires a window with orderBy().
+def part_one(data):
+    return count_increases(data)
 
-# I want to keep the data in the original order, so I need to add a column with
-# the original sequence
-data = input_data.withColumn('seq', f.monotonically_increasing_id())
+def part_two(data):
+    # For part 2, I need to compute a 3-sample moving average first
+    
+    # Three sample window (current sample and next two)
+    threes = pyspark.sql.Window.orderBy('seq').rowsBetween(pyspark.sql.Window.currentRow, 2)
+    
+    smoothed = data.select(
+        f.min(data['seq']).over(threes).alias('seq'),
+        f.sum(data['depth']).over(threes).alias('depth')
+    )
 
-seq_window = pyspark.sql.Window.orderBy('seq')
-data = data.withColumn('prev_depth', f.lag(data['depth']).over(seq_window))
+    # Remove samples at the end with incomplete windows
+    smoothed = smoothed.filter(smoothed['seq'] < (smoothed.count() - 2))
 
-# Finally, solve the problem! Count the number of measurements that are larger
-# than the previous measurement
-ans = data.filter(data['depth'] > data['prev_depth']).count()
-print(ans)
+    return count_increases(smoothed)
+
+if __name__ == '__main__':
+    # Usual setup
+    spark = pyspark.sql.SparkSession.builder.getOrCreate()
+    input_filename = '/Users/stetner/adventofcode/2021/day/01/input.txt'
+    # input_filename = '/Users/stetner/adventofcode/2021/day/01/example.txt'
+    # input_filename = sys.argv[1]
+    
+    data = load_data(spark, input_filename)
+    print('--------')
+    print('The answer to part one is: {}'.format(part_one(data)))
+    print('--------')
+    print('The answer to part two is: {}'.format(part_two(data)))
